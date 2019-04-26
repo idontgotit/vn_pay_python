baa1053d16c54604db6f49d41db492ebbaf52d99
idontgotit <idontgotit@gmail.com>
2019-04-26 16:29:45 +0700

    Update settings.py

--- vnpay_python/settings.py
+++ vnpay_python/settings.py
@@ -121,5 +121,5 @@ STATIC_URL = '/static/'
 VNPAY_RETURN_URL = 'http://localhost:8000/payment_return'  # get from config
 VNPAY_PAYMENT_URL = 'http://sandbox.vnpayment.vn/paymentv2/vpcpay.html'  # get from config
 VNPAY_API_URL = 'http://sandbox.vnpayment.vn/merchant_webapi/merchant.html'
-VNPAY_TMN_CODE = ''  # Website ID in VNPAY System, get from config
-VNPAY_HASH_SECRET_KEY = ''  # Secret key for create checksum,get from config
+VNPAY_TMN_CODE = 'UNVORBNW'  # Website ID in VNPAY System, get from config
+VNPAY_HASH_SECRET_KEY = 'BALYAITQPFXMRLPNDICQSHZHFHCBTDOL'  # Secret key for create checksum,get from config
